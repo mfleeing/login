29644517399f133e03743dc59ec339113014407d
mfleeing <1769240409@qq.com>
2019-12-06 19:32:24 +0800

    这是我更新之后的登录注册,验证码接口

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var svgCaptcha = require('svg-captcha');
 var fs = require('fs');
+var express = require('express');
 var app = express();
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     database: 'test'
 });
 
-
+var svgCaptcha = require('svg-captcha');
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -20,7 +20,6 @@ app.all('*', function(req, res, next) {
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
-
 
 
 app.get('/', function(req, res) {
@@ -37,7 +36,7 @@ app.get('/', function(req, res) {
 app.get('/getdata', function(req, res) {
     connection.query('select * from datas', function(err, data) {
         if (err) {
-            console.log('有错误,原因是' + err)
+            console.log('有错误,原因是' + err);
         } else {
             res.send(data)
         }
@@ -45,75 +44,61 @@ app.get('/getdata', function(req, res) {
 });
 
 
-
 var yzmnum = '';
 app.get('/yzm', function(req, res) {
-    var captcha = svgCaptcha.create({ fontSize: 50, width: 100, height: 40, noise: 3, color: true, background: '#cc9966' });
+    var captcha = svgCaptcha.create({ fontSize: 50, width: 100, height: 40, noise: 6, color: true, background: '#cc9966', ignoreChars: '0o1i' });
     res.setHeader('Content-Type', 'image/svg+xml');
     res.send(captcha.data);
-    console.log(new Date().toLocaleString() + "验证码生成成功" + captcha.text);
+    console.log(new Date().toLocaleString() + "验证码生成成功  : " + captcha.text);
     yzmnum = captcha.text;
 });
 
-
+// 参数定义  1.参数不完整
+//          2.成功
+//          3.失败
 
 app.get('/login', function(req, res) {
-    //判断url请求是否携带完整参数
-    if (!req.query.name || !req.query.password || !req.query.yzm) {
-        let information = { code: 1, message: '登录缺少必要的参数,用户名或密码或验证码为空' }
-        res.json(information);
+    var information = '';
+    if (req.query.yzm.toLowerCase() != yzmnum.toLowerCase()) {
+        information = { code: 3, message: '验证码错误' };
+        console.log(information);
+        res.send(information);
     } else {
+        //若验证码正确,连接数据库,进行验证
         connection.query('select * from username', function(err, data) {
             if (err) {
                 console.log('有错误,原因是' + err)
             } else {
-                var information = '';
+                //连接数据库成功,进行用户名遍历是否存在用户名
                 for (let i = 0; i < data.length; i++) {
                     if (req.query.name != data[i].name) {
-                        information = { code: 4, message: '用户名不存在' };
-                    } else {
-                        if (req.query.name == data[i].name && req.query.password == data[i].password) {
-                            information = { code: 2, message: '登录成功' };
-                            break;
-                        } else {
-                            information = { code: 3, message: '密码错误' };
-                            break;
-                        }
+                        information = { code: 3, message: '用户名不存在' };
+                    } else if (req.query.name == data[i].name && req.query.password == data[i].password) {
+                        information = { code: 2, message: '登录成功' };
+                        break;
+                    } else if (req.query.name == data[i].name && req.query.password != data[i].password) {
+                        information = { code: 3, message: '密码错误' };
+                        break;
                     }
-                };
+                }
                 console.log(information);
-                res.json(information);
-
-                // data.forEach(element => {
-                //     console.log(this);
-                //     if (req.query.name != element.name) {
-                //         information = { code: 4, message: '用户名不存在' };
-                //     } else {
-                //         if (req.query.name == element.name && req.query.password == element.password) {
-                //             information = { code: 2, message: '登录成功' };
-                //         } else {
-                //             information = { code: 3, message: '密码错误' };
-                //         }
-                //     }
-                // });
-                // console.log(information);
-                // res.json(information);
+                res.send(information);
             }
-
         });
-
-    };
+    }
 
 });
 
 
 app.get('/register', function(req, res) {
     if (!req.query.name || !req.query.password || !req.query.nc) {
-        let data = { code: 1, message: '注册缺少必要的参数,用户名或密码或昵称为空' }
-        res.json(data);
+        let information = { code: 1, message: '注册缺少必要的参数,用户名或密码或昵称为空' };
+        console.log(information);
+        res.send(information);
     } else if (req.query.name.length < 6 || req.query.password.length < 6 || req.query.nc.length < 6) {
-        let information = { code: 5, message: '用户名或密码或昵称的长度不小于6位' };
-        res.json(information);
+        let information = { code: 1, message: '用户名或密码或昵称的长度不小于6位' };
+        console.log(information);
+        res.send(information);
     } else {
         //若满足注册条件,连接数据库进行查询,查看数据库中是否已存在相同用户名
         connection.query('select * from username', function(err, data) {
@@ -123,7 +108,7 @@ app.get('/register', function(req, res) {
                 //若连接数据库正常.遍历数据库中的数据,查看数据库中是否已存在相同用户名
                 for (let i = 0; i < data.length; i++) {
                     if (req.query.name == data[i].name) {
-                        information = { code: 4, message: '用户名已存在' };
+                        information = { code: 3, message: '用户名已存在' };
                         break;
                     } else {
                         //若数据库中未有此用户名,即可进行插入操作
@@ -133,17 +118,13 @@ app.get('/register', function(req, res) {
                                 return;
                             }
                         })
-                        information = { code: 5, message: '用户名注册成功' };
+                        information = { code: 2, message: '用户名注册成功' };
                     }
                 };
                 console.log(information);
-                res.json(information);
+                res.send(information);
             }
-
         });
-
-
-
     }
 });
 
